48331c26ba5cc228261789cd582872e4a9686b72
Younis Amedi <4110665+younisamedi@users.noreply.github.com>
2021-05-10 17:45:43 -0500

    Update connect_sxp.sh

--- connect_sxp.sh
+++ connect_sxp.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Copyright (c) 2019 - Younis Amedi < ya@younisamedi.com >
+# Copyright (c) 2019 - Younis Amedi < www.younisamedi.com >
 # This script is licensed under GNU GPL version 2.0 or above
 #=========================================================================================
 #title           : connect_sxp.sh
