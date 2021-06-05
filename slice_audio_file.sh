2a105c53db17084372a32769f2041e9cd038933c
Younis Amedi <4110665+younisamedi@users.noreply.github.com>
2021-06-05 09:31:35 -0500

    Create slice_audio_file.sh

---
+++ slice_audio_file.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+# Younis Amedi
+# July 2018
+# Must have "ffmpeg" installed
+# This works with all the common audio format
+##############
+
+### Provide an audio file
+if [ -z "$1" ]
+   then
+	   echo "must have an audio file as a first argument. E.g: ./slice_audio_file.sh music.mp3"
+	   exit 1
+fi
+
+
+file_extension=$(echo $1 | tail -c 4)
+
+### 1800 means 30 minutes
+ffmpeg -i "$1" -f segment -segment_time 3600 "${1::-4}"%03d.$file_extension
+
+exit
+
+### End
